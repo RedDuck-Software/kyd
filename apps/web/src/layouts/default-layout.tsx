c5f6f67525a4d6d747079e813587344c156fed87
MrJeleika <den932005@gmail.com>
2024-06-21 18:18:22 +0300

    feat(web): images

--- apps/web/src/layouts/default-layout.tsx
+++ apps/web/src/layouts/default-layout.tsx
@@ -10,16 +10,20 @@ import { Outlet } from 'react-router-dom';
 const DefaultLayout = memo(() => {
   return (
     <Providers>
-      <div className="min-h-screen flex flex-1 max-w-[1375px] mx-auto flex-col px-2 sm:px-4">
-        <Header />
-        <main className="flex-1">
-          <Suspense fallback={<PageLoader screen />}>
-            <Outlet />
-          </Suspense>
-        </main>
-        <Footer />
-        <Toaster />
-        <Modals />
+      <div className="relative overflow-hidden">
+        <img src="/images/top-bg.png" className="-z-10 w-full absolute -top-[60px] left-0" alt="" />
+        <img src="/images/bottom-bg.png" className="-z-10 w-full absolute -bottom-[60px] left-0" alt="" />
+        <div className="min-h-screen  flex flex-1 max-w-[1375px] mx-auto flex-col px-2 sm:px-4">
+          <Header />
+          <main className="flex-1">
+            <Suspense fallback={<PageLoader screen />}>
+              <Outlet />
+            </Suspense>
+          </main>
+          <Footer />
+          <Toaster />
+          <Modals />
+        </div>
       </div>
     </Providers>
   );
