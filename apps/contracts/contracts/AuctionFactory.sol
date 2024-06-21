a51dd3dae52eb67bee4d5ec195e6a3e05282eb9f
kostyamospan <kostya04042001@gmail.com>
2024-06-21 12:06:51 +0300

    choree: auction factory

--- apps/contracts/contracts/AuctionFactory.sol
+++ apps/contracts/contracts/AuctionFactory.sol
@@ -1,12 +1,31 @@
-// 
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.23;
 
-struct AuctionCreateParams { }
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "./interfaces/IAuction.sol";
+import "./Auction.sol";
+
+struct AuctionCreateParams {
+    address owner;
+}
 
 contract AuctionFactory {
+    address[] public auctions;
 
-    constructor( ) {}
+    event AuctionDeployed { 
+        address indexed auction
+    }
 
-    public create(AuctionCreateParams calldata params) { 
+    constructor() {}
 
+    function create(AuctionCreateParams calldata params) external {
+        Auction auction = Auction(payable(Clones.clone(implementation)));
+        auction.initialize(AuctionCreateParams({owner: owner}));
+
+        auctions.push(address(auction));
+
+        emit AuctionDeployed(address(auction));
+        
+        return address(auction);
     }
 }
