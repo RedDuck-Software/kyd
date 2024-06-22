e2df4dd31bbf07e1b136c4eee6e46f04cc40557c
m000nler <oleksii.rychko@redduck.io>
2024-06-22 20:35:39 +0300

    fix: graph timestamp fix

--- apps/contracts/contracts/Auction.sol
+++ apps/contracts/contracts/Auction.sol
@@ -210,9 +210,11 @@ contract Auction is IAuction, OwnableUpgradeable, GelatoVRFConsumerBase {
                     DoubleLinkedList.Data({user: msg.sender, amount: amount})
                 );
             }
-        }
 
-        userDonated[msg.sender] = true;
+            _mint(nftParticipate, msg.sender, 0);
+
+            userDonated[msg.sender] = true;
+        }
 
         emit Donate(stable, msg.sender, amount);
 
--- apps/contracts/test/common/constants.ts
+++ apps/contracts/test/common/constants.ts
@@ -1,5 +1,4 @@
 import { DeploymentConstans, NETWORK } from '@/types/constant.types';
-import { AddressLike } from 'ethers';
 import { Address, encodePacked } from 'viem';
 
 const ethUsdPriceFeed: Address = '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419';
