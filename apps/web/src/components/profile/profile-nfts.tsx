37b5550448cf4f8752cac88047e5e90609b21350
Dmytro Horbatenko <dmytro.horbatenko@redduck.io>
2024-06-23 02:52:50 +0300

    feat: profile nfts initial setup

--- apps/web/src/components/profile/profile-nfts.tsx
+++ apps/web/src/components/profile/profile-nfts.tsx
@@ -1,13 +1,52 @@
 import { routes } from '@/router';
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ShadowCard } from '../common/shadow-card';
 import { getShadowCardFilledVariant } from '@/lib/shadow-card-variant';
-import { useGetUserNFTs } from '@/hooks/queries/use-user-nfts';
+import { useDefaultSubgraphQuery } from '@/hooks/useDefaultSubgraphQuery';
+import { GET_USER_NFTS, GetUserNftsResponse } from '@/graph/queries/get-user-nfts';
+import { useAccount } from 'wagmi';
+import { httpClient } from '@/api/client';
 
-export const ProfileNfts = () => {
-  const { data: userNftsRes } = useGetUserNFTs();
+interface NftMetadata {
+  description: string;
+  imageUrl: string;
+}
+
+export const ProfileNfts: React.FC = () => {
   const navigate = useNavigate();
+  const { address } = useAccount();
+
+  const { data } = useDefaultSubgraphQuery<GetUserNftsResponse>(GET_USER_NFTS, { owner: address });
+
+  const [nftData, setNftData] = useState<{ [key: string]: NftMetadata }>({});
+
+  useEffect(() => {
+    const fetchNftData = async () => {
+      const fetchedNftData: { [key: string]: NftMetadata } = {};
+
+      if (!data?.auctionNFTs) return;
+
+      await Promise.all(
+        data.auctionNFTs.map(async (nft) => {
+          try {
+            const response = await httpClient.get<NftMetadata>(`${nft.uri}0`);
+            if (response.data) {
+              fetchedNftData[nft.id] = response.data;
+            } else {
+              console.error(`Failed to fetch data for NFT ${nft.id}: ${response.statusText}`);
+            }
+          } catch (error) {
+            console.error(`Error fetching data for NFT ${nft.id}:`, error);
+          }
+        })
+      );
+
+      setNftData(fetchedNftData);
+    };
+
+    fetchNftData();
+  }, [data?.auctionNFTs]);
 
   const handleNftClick = useCallback(() => {
     navigate(routes.nft);
@@ -17,18 +56,25 @@ export const ProfileNfts = () => {
     <div className="flex flex-col gap-4">
       <h2 className="text-[32px] font-semibold">My NFTs</h2>
       <div className="grid lg:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-6">
-        {userNftsRes?.map((nft, i) => (
-          <button onClick={handleNftClick} className="">
-            <ShadowCard
-              variant={getShadowCardFilledVariant(i)}
-              className="flex flex-col overflow-hidden items-center gap-2"
-            >
-              <img src={nft.img} alt="" className="rounded-[16px]" />
-              <h6 className="text-[18px] font-medium">{nft.name}</h6>
-              <p>{nft.description}</p>
-            </ShadowCard>
-          </button>
-        ))}
+        {data?.auctionNFTs?.length ? (
+          data.auctionNFTs.map((nft, i) => (
+            <button key={nft.id} onClick={handleNftClick} className="">
+              <ShadowCard
+                variant={getShadowCardFilledVariant(i)}
+                className="flex flex-col overflow-hidden items-center gap-2"
+              >
+                <img
+                  src={nftData[nft.id]?.imageUrl}
+                  alt={nftData[nft.id]?.description || ''}
+                  className="rounded-[16px]"
+                />
+                <h6 className="text-[18px] font-medium">{nftData[nft.id]?.description}</h6>
+              </ShadowCard>
+            </button>
+          ))
+        ) : (
+          <div>You don't have any NFTs :(</div>
+        )}
       </div>
     </div>
   );
--- apps/web/src/graph/queries/get-user-nfts.ts
+++ apps/web/src/graph/queries/get-user-nfts.ts
@@ -1,41 +1,30 @@
-import { Address } from 'viem';
-import { getGraphClients } from '../client';
 import { gql } from '@apollo/client';
-
-export interface NFTBurn {
-  from: string;
-  tokenId: number;
-  id: string;
-}
+import { Address } from 'viem';
 
 export interface NFT {
-  name: string;
-  img: string;
-  description: string;
+  id: string;
+  address: Address;
+  owner: Address;
+  tokenId: string;
+  transactionHash: string;
+  blockNumber: string;
+  blockTimestamp: string;
 }
 
-export const getUserNfts = async (address: Address) => {
-  const clients = getGraphClients();
-  const queries = clients.map((client) =>
-    client.query({
-      query: gql`
-        {
-          AuctionNFTBurn(where: { from: "${address}" }){
-            from
-            tokenId
-            id
-          },
-          AuctionNFT1155Burn(where: { from: "${address}" }){
-            from
-            tokenId
-            id
-          }
-        }
-      `,
-    })
-  );
+export interface GetUserNftsResponse {
+  auctionNFTs: NFT[];
+}
 
-  const results = await Promise.all(queries);
-
-  console.log(results);
-};
+export const GET_USER_NFTS = gql`
+  query GetUserNfts($owner: Bytes) {
+    auctionNFTs(where: { owner: $owner }) {
+      id
+      address
+      owner
+      tokenId
+      blockNumber
+      blockTimestamp
+      transactionHash
+    }
+  }
+`;
