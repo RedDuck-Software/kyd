ca819555c9bdc447676656cc244c4ca32538fafe
Dmytro Horbatenko <dmytro.horbatenko@redduck.io>
2024-06-23 02:08:57 +0300

    fix: lint

--- apps/web/src/pages/home.tsx
+++ apps/web/src/pages/home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { httpClient } from '@/api/client';
 import { ShadowCard } from '@/components/common/shadow-card';
 import { TrandingAuctions } from '@/components/home/tranding-auctions';
