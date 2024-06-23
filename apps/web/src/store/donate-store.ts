9111cee8241a5e57fdc0c703463f368bf9d9b17f
MrJeleika <den932005@gmail.com>
2024-06-23 07:59:37 +0300

    fix: chain icons

--- apps/web/src/store/donate-store.ts
+++ apps/web/src/store/donate-store.ts
@@ -3,41 +3,35 @@ import { TokenBalance } from '@/hooks/useTokenBalances';
 import { maxInt256 } from 'viem';
 import { create } from 'zustand';
 import { allowedTokens } from '../constants/addresses';
+import { polygon, scrollSepolia, sepolia } from 'viem/chains';
 
 export interface AllowedDonateChain {
-  id: AllowedChainIds;
   name: string;
   image: string;
 }
 
-export const allowedChains = [
-  {
-    id: 534351,
+export const allowedChains = {
+  [scrollSepolia.id]: {
     name: 'Scroll sepolia',
     image: 'https://avatars.githubusercontent.com/u/87750292?s=280&v=4',
   },
-  {
-    id: 80001,
-    name: 'Polygon mumbai',
+  [polygon.id]: {
+    name: 'Polygon',
     image:
       'https://moralis.io/wp-content/uploads/web3wiki/116-mumbai/637adca2e1a09547acd85968_Y_44LwHNRnOEvnRExgnO1UujtZwn7zq7BCb4oxxHgpI.jpeg',
   },
-] as AllowedDonateChain[];
+  [sepolia.id]: {
+    name: 'Sepolia',
+    image: 'https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=032',
+  },
+} as Record<AllowedChainIds, AllowedDonateChain>;
 
 interface DonateStore {
-  chain: AllowedDonateChain;
-  setDonateChain: (chain: AllowedDonateChain) => void;
-  token: TokenBalance;
-  setToken: (token: TokenBalance) => void;
   amount: number | null;
   setAmount: (amount: number | null) => void;
 }
 
 const useDonateStore = create<DonateStore>((set) => ({
-  chain: allowedChains[0],
-  setDonateChain: (chain) => set({ chain }),
-  token: { ...allowedTokens[allowedChains[0].id][0], balance: BigInt(0), allowance: maxInt256 },
-  setToken: (token) => set({ token }),
   amount: 0,
   setAmount: (amount) => set({ amount }),
 }));
