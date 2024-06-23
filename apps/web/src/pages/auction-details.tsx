91869f808e9018ba34e30c1c82d57151799dbf20
MrJeleika <den932005@gmail.com>
2024-06-23 06:51:43 +0300

    fix: design

--- apps/web/src/pages/auction-details.tsx
+++ apps/web/src/pages/auction-details.tsx
@@ -6,12 +6,13 @@ import { AuctionProgress } from '@/components/auction/auction-progress';
 import { useAuctionMetadata } from '@/hooks/queries/use-auction-metadata';
 import { useMemo } from 'react';
 import { AuctionMetadata } from './home';
+import { PageLoader } from '@/components/page-loader/page-loader';
 
 export default function AuctionDetails() {
   const { id } = useParams();
   const [address, auctionChainId] = id!.split(':');
 
-  const { data } = useAuctionMetadata(address, +auctionChainId);
+  const { data, isLoading } = useAuctionMetadata(address, +auctionChainId);
 
   const metadata: AuctionMetadata = useMemo(() => {
     return data?.data ?? { description: '', name: '', uri: '' };
@@ -20,13 +21,19 @@ export default function AuctionDetails() {
   return (
     <div className="flex flex-col gap-16">
       <div className="flex flex-col gap-4">
-        <div className="flex flex-col gap-2">
-          <h1 className="text-4xl font-semibold">{metadata.name}</h1>
-          <p className="text-base">{metadata.description}</p>
-        </div>
-        <div className="flex justify-center">
-          <img className="max-w-[500px]" src={metadata.uri} />
-        </div>
+        {isLoading || !data ? (
+          <PageLoader />
+        ) : (
+          <>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-4xl font-semibold">{metadata.name}</h1>
+              <p className="text-base">{metadata.description}</p>
+            </div>
+            <div className="flex justify-center">
+              <img className="max-w-[500px] aspect-square object-cover" src={metadata.uri} />
+            </div>
+          </>
+        )}
       </div>
       <AuctionProgress />
       <AuctionDonate />
