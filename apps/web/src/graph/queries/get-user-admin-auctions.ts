fbad0c1dfcf8466c42bdc29a7059fce72cbcda91
m000nler <oleksii.rychko@redduck.io>
2024-06-23 05:27:12 +0300

    chore: admin auctions wip

---
+++ apps/web/src/graph/queries/get-user-admin-auctions.ts
@@ -0,0 +1,35 @@
+import { gql } from '@apollo/client';
+import { Address } from 'viem';
+import { getGraphClients } from '../client';
+
+export const getUserAdminAuctions = async (user: Address) => {
+  const clients = getGraphClients();
+
+  console.log({ user });
+
+  user = '0x6e2f1d7ee2aa23007448810bd21bbccce07ff21c';
+
+  const queries = await Promise.all(
+    clients.map(async (client) => ({
+      result: client.query({
+        query: gql`
+        {
+            auctionCreateds(where: { owner: "${user}" }) {
+              id
+              address
+              uri
+              owner
+              blockNumber
+              blockTimestamp
+              transactionHash
+            }
+        }
+        `,
+      }),
+    }))
+  );
+
+  const results = await Promise.all(queries);
+
+  return Object.entries(results);
+};
